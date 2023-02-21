5768f3dbf2e65850e7ef2cb0001008a75461211f
Kevin Sinaga <kevinsinaga11.ks@gmail.com>
2023-02-22 00:51:59 +0700

    adding user

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -24,7 +24,7 @@ class UserController extends Controller
      */
     public function create()
     {
-        return view('admin.user.create');
+        return view('admin.user.form');
     }
 
     /**
